ec64b38745d79b2583ea8cc4e485a5436e5dede7
arthurkz <arthurribeirokz@gmail.com>
2022-10-15 14:34:45 -0300

    feat: primeiro comentario

---
+++ src/main/java/geek/search/api/GeekSearchApiApplication.java
@@ -0,0 +1,13 @@
+package geek.search.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geek/search/api/GeekSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package geek.search.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
